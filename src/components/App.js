bfb2a831dec61e494595d5667aa65dc4208e68f5
Daniel Cox <dan.cox@connect.auto>
2018-08-24 12:35:29 +0100

    React Plaground set up

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>React with Babel and Webpack</h1>
+                <h1>React Playground</h1>
             </div>
         );
     }
