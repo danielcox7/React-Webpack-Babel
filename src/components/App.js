3bdcb1003ca4ec8484a12d0dc6bcf9eedae9fe6e
Dan Cox <Dan@latitudeE5450-252.office.motortrak.com>
2018-09-12 00:29:44 +0100

    Updated Readme

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>React Playground</h1>
+                <h1>Dan's React Playground...........</h1>
             </div>
         );
     }
