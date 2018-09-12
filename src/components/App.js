777b0d8e868ccb842502070d130f743d2032fa34
Dan Cox <Dan@latitudeE5450-252.office.motortrak.com>
2018-09-12 23:46:43 +0100

    DC-1, playing with react

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 
 import '../styles/App.css';
+import '../styles/Header.css';
 
-class App extends Component {
+class App extends React.Component {
     render() {
-        return (
-            <div>
-                <h1>Dan's React Playground...........</h1>
-            </div>
-        );
+       return (
+          <div>
+             <Header/>
+             <Pagetitle/>
+          </div>
+       );
     }
-}
+ }
+ class Header extends React.Component {
+    render() {
+       return (
+            <header id="header">Header</header>
+       );
+    }
+ }
+
+ class Pagetitle extends React.Component {
+    render() {
+       return (
+          <h1>
+            Page Title
+          </h1>
+       );
+    }
+ }
 
 export default App;
