06282ec5d5849930203d5c76c033f8498f330711
Daniel Cox <dannyjohncox@hotmail.com>
2018-09-12 23:52:36 +0100

    Dc 1 (#2)
    * Updated Readme
    
    * DC-1, playing with react
    
    * DC-1, fix conflicts

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import '../styles/Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header id="header">Header</header>
+        );
+    }
+}
+
+export default Header;
